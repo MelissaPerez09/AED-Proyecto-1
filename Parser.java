d296ca21989be3089b89e3caa3021161fb047df2
Mark Albrand <mark.albrand5@gmail.com>
2022-03-03 18:32:51 -0600

    Estructura del parser

---
+++ Parser.java
@@ -0,0 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    public String parse(String linea){
+        Pattern pattern;
+        Matcher matcher;
+
+        // Operaciones aritméticas simples
+        pattern = Pattern.compile("^[(]{1}[+*-/]+ [0-9]+ [0-9]+[)]{1}", Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(linea);
+
+        if(matcher.find()){
+            String[] datos = linea.split(" ");
+            String a = datos[1];
+            String b = datos[2];
+
+            if(a.contains(".") || b.contains(".")){
+                // Número con decimales
+                double x = Double.parseDouble(a);
+                double y = Double.parseDouble(b);
+
+
+            } else{
+                // Número entero
+                int x = Integer.parseInt(a);
+                int y = Integer.parseInt(b);
+            }
+        }
+
+    }
+}
