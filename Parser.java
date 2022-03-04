eafe28f3208561437e0aa511fb271282799bdaa2
Mark Albrand <mark.albrand5@gmail.com>
2022-03-03 18:38:33 -0600

    Manejo de error si no se encuentra una expresión válida

--- Parser.java
+++ Parser.java
@@ -7,7 +7,7 @@ public class Parser {
         Matcher matcher;
 
         // Operaciones aritméticas simples
-        pattern = Pattern.compile("^[(]{1}[+*-/]+ [0-9]+ [0-9]+[)]{1}", Pattern.CASE_INSENSITIVE);
+        pattern = Pattern.compile("^[(]{1}[+*-/]+ [0-9]+ [0-9]+[)]{1}$", Pattern.CASE_INSENSITIVE);  // Regex para una operación simple
         matcher = pattern.matcher(linea);
 
         if(matcher.find()){
@@ -20,13 +20,15 @@ public class Parser {
                 double x = Double.parseDouble(a);
                 double y = Double.parseDouble(b);
 
-
+                return "";
             } else{
                 // Número entero
                 int x = Integer.parseInt(a);
                 int y = Integer.parseInt(b);
+
+                return "";
             }
         }
-
+        return "Expresión inválida. Ingrese (EXIT) para salir.";
     }
 }
