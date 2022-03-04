3f54bdf11d3a86db080ac8b5925657144bd2e1bf
Mark Albrand <mark.albrand5@gmail.com>
2022-03-03 19:23:19 -0600

    Increase y decrease

--- Parser.java
+++ Parser.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,28 +8,75 @@ public class Parser {
         Matcher matcher;
 
         // Operaciones aritméticas simples
-        pattern = Pattern.compile("^[(]{1}[+*-/]+ [0-9]+ [0-9]+[)]{1}$", Pattern.CASE_INSENSITIVE);  // Regex para una operación simple
+        pattern = Pattern.compile("^[(]{1}[+*-/]{1} [0-9]+ [0-9]+[)]{1}$", Pattern.CASE_INSENSITIVE);  // Regex para una operación simple
         matcher = pattern.matcher(linea);
 
         if(matcher.find()){
             String[] datos = linea.split(" ");
-            String a = datos[1];
-            String b = datos[2];
+            String a = datos[2];
+            String b = datos[3];
 
             if(a.contains(".") || b.contains(".")){
                 // Número con decimales
                 double x = Double.parseDouble(a);
                 double y = Double.parseDouble(b);
 
-                return "";
+                double resultado = Aritmeticos.add(x, y);
+
+                return Double.toString(resultado);
             } else{
                 // Número entero
                 int x = Integer.parseInt(a);
                 int y = Integer.parseInt(b);
 
-                return "";
+                int resultado = Aritmeticos.add(x, y);
+
+                return Integer.toString(resultado);
             }
+
         }
+        // Operaciones aritméticas simples (Una variable)
+        pattern = Pattern.compile("^[(]{1}[+\\-]{2} [0-9]+[)]{1}$\n", Pattern.CASE_INSENSITIVE);  // Regex para una operación simple
+        matcher = pattern.matcher(linea);
+
+        if(matcher.find()){
+            String[] datos = linea.split(" ");
+            String a = datos[1];
+
+            if(Objects.equals(datos[1], "++")){
+                if(a.contains(".")){
+                    double x = Double.parseDouble(a);
+
+                    double resultado = Aritmeticos.incr(x);
+
+                    return Double.toString(resultado);
+                }else{
+                    int x = Integer.parseInt(a);
+
+                    int resultado = Aritmeticos.incr(x);
+
+                    return Integer.toString(resultado);
+                }
+            }else {
+                if(a.contains(".")){
+                    double x = Double.parseDouble(a);
+
+                    double resultado = Aritmeticos.decr(x);
+
+                    return Double.toString(resultado);
+                }else{
+                    int x = Integer.parseInt(a);
+
+                    int resultado = Aritmeticos.decr(x);
+
+                    return Integer.toString(resultado);
+                }
+            }
+
+
+        }
+
+
         return "Expresión inválida. Ingrese '(EXIT)' para salir.";
     }
 }
