834c214e79cfcf720d23a105997b128479a427e7
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-03-23 16:58:00 -0600

    cambio

--- Vista.java
+++ Vista.java
@@ -67,7 +67,7 @@ public class Vista {
         System.out.println("\n" +"···");
                 
         linea = scan.nextLine();
-        while(linea != ("END")){
+        while(!linea.equals(("END"))){
             System.out.println("\n" +"···");
             funcion.add(linea);
         } 
